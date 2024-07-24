6246b772f3133e7b540eeee85eedf623e4353f0a
muhammedhuseynli <huseynlimehemmed08@gmail.com>
2024-07-24 12:06:19 +0400

    added basket

--- src/js/basket.js
+++ src/js/basket.js
@@ -1,11 +1,18 @@
 import "../css/basket.css";
+import basketIcon from "../icons/basketIcon.svg";
 
-function Basket(){
-    return(
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+function Basket() {
+  return (
+    <div className="basketMainDiv">
+      <img src={basketIcon} width={64} height={64}></img>
+      <h1>Your basket is empty!</h1>
+      <p>
+        To fill your cart with the products and opportunites of Onsrashop.com,<br></br>
+        you may now start discover our products.
+      </p>
+      <button>Continue Shopping</button>
+    </div>
+  );
 }
 
 export default Basket;
